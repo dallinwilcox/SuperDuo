440d3443c6a3869cc835a5e6038bcab14c8d3db7
dallinwilcox <dcwilcox@gmail.com>
2016-03-02 23:17:55 -0600

    working widget with cursor

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/FootbalScoresAppWidgetProvider.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/FootbalScoresAppWidgetProvider.java
@@ -6,8 +6,10 @@ import android.appwidget.AppWidgetProvider;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
 
+import barqsoft.footballscores.service.ScoreWidgetService;
 import barqsoft.footballscores.service.myFetchService;
 //referenced http://developer.android.com/guide/topics/appwidgets/index.html
 /**
@@ -16,22 +18,25 @@ import barqsoft.footballscores.service.myFetchService;
 public class FootbalScoresAppWidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int N = appWidgetIds.length;
-        //invoke Widget Service
-//        Intent service_start = new Intent(context, myFetchService.class);
-//        context.startService(service_start);
+
 
         // Perform this loop procedure for each App Widget that belongs to this provider
         for (int i = 0; i < N; i++) {
             int appWidgetId = appWidgetIds[i];
+            //invoke Widget Service
+            Intent serviceIntent =  new Intent(context, ScoreWidgetService.class);
+            serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
 
             // Create an Intent to launch ExampleActivity
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            Intent clickIntent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, clickIntent, 0);
 
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.scores_list_item);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
             views.setOnClickPendingIntent(R.id.wrapper, pendingIntent);
+            views.setRemoteAdapter(appWidgetId, R.id.widget_score_list, serviceIntent);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/service/ScoreWidgetService.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/service/ScoreWidgetService.java
@@ -5,8 +5,8 @@ import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.os.Bundle;
 import android.support.v4.content.ContentResolverCompat;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
@@ -14,7 +14,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import barqsoft.footballscores.DatabaseContract;
-import barqsoft.footballscores.FootbalScoresAppWidgetProvider;
 import barqsoft.footballscores.R;
 import barqsoft.footballscores.Utilies;
 import barqsoft.footballscores.scoresAdapter;
@@ -22,7 +21,6 @@ import barqsoft.footballscores.scoresAdapter;
 /**
  * Created by dcwilcox on 2/15/2016.
  */
-@TargetApi(11)
 public class ScoreWidgetService extends RemoteViewsService{
 
     @Override
@@ -33,6 +31,8 @@ public class ScoreWidgetService extends RemoteViewsService{
     class ScoreListRemoteViewsFactory implements RemoteViewsFactory {
         private Context context;
         private int appWidgetId;
+        private Cursor cursor;
+        private int scoreCount;
         public ScoreListRemoteViewsFactory(Context context, Intent intent)
         {
             this.context = context;
@@ -41,8 +41,12 @@ public class ScoreWidgetService extends RemoteViewsService{
         }
         @Override
         public void onCreate() {
-
-
+            Log.d("ScoreWidgetService", "created");
+            SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
+            String[] currentDate = new String[1];
+            currentDate[0] = mformat.format(new Date(System.currentTimeMillis()));
+            cursor  = ContentResolverCompat.query(context.getContentResolver(),DatabaseContract.scores_table.buildScoreWithDate(),null,null,currentDate,null,null);
+            scoreCount = cursor.getCount();
         }
 
         @Override
@@ -52,27 +56,24 @@ public class ScoreWidgetService extends RemoteViewsService{
 
         @Override
         public void onDestroy() {
-
+            if (null != cursor){cursor.close();}
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return scoreCount;
         }
 
         @Override
         public RemoteViews getViewAt(int position) {
-
-            SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
-            String[] currentDate = new String[1];
-            currentDate[0] = mformat.format(new Date(System.currentTimeMillis()));
-            Cursor cursor  = ContentResolverCompat.query(context.getContentResolver(),DatabaseContract.scores_table.buildScoreWithDate(),null,null,currentDate,null,null);
+            Log.d("ScoreWidgetService", "getViewAt: " + position);
+            cursor.moveToPosition(position);
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.scores_list_item);
+            Log.d("ScoreWidgetService", "setting home_name to " + cursor.getString(scoresAdapter.COL_HOME));
             remoteViews.setTextViewText(R.id.home_name, cursor.getString(scoresAdapter.COL_HOME));
             remoteViews.setTextViewText(R.id.away_name, cursor.getString(scoresAdapter.COL_AWAY));
             remoteViews.setTextViewText(R.id.score_textview, Utilies.getScores(cursor.getInt(scoresAdapter.COL_HOME_GOALS),cursor.getInt(scoresAdapter.COL_AWAY_GOALS)));
             remoteViews.setTextViewText(R.id.data_textview, cursor.getString(scoresAdapter.COL_MATCHTIME));
-
             return remoteViews;
         }
 
